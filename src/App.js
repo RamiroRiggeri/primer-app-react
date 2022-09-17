72495e088f897583ae651ece6d1d52ceec5c605d
Ramiro Riggeri <ramiro.riggeri@gmail.com>
2022-09-17 17:58:07 -0300

    primer subida

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import Message from "./Mensaje";
+const App = () => {
+  const Description = () => {
+    return <p>Y esta la descripción</p>;
+  };
+  //esto funciona, pero no es recomendable, lo recomendable es crear
+  //los componentes en su propio archivo
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message color="green" msg="Mi primer App en React" />
+      <Message msg="pos no sé" />
+      <Message color="red" msg="es React" />
+      <Description />
     </div>
   );
-}
+};
 
 export default App;
